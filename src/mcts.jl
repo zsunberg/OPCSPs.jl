eb63b09feceb3d1cf56d8fd8d61f166062bf3bc0
Zachary Sunberg <sunbergzach@gmail.com>
2016-02-29 21:43:02 -0800

    faster mapping for aggregator

--- src/mcts.jl
+++ src/mcts.jl
@@ -59,61 +59,48 @@ function MCTS.next_action(gen::PreSolvedActionGenerator, mdp::OPCSPBeliefMDP, s:
     return move
 end
 
-type OPCSPAg <: MCTS.Aggregator 
+type VaroniOPCSPAg <: MCTS.Aggregator 
     radius::Float64
-    anchors::Dict{Int,Vector{OPCSPBelief}}
+    anchors::Dict{Tuple{Int,IntSet},Vector{OPCSPBelief}}
 end
-OPCSPAg(radius::Float64) = OPCSPAg(radius, Dict{Int,Vector{OPCSPBelief}}())
+VaroniOPCSPAg(radius::Float64) = VaroniOPCSPAg(radius, Dict{Int,Vector{OPCSPBelief}}())
 
 # inlined below
 same_besides_profit(u::OPCSPBelief, v::OPCSPBelief) = u.i == v.i && u.remaining == v.remaining && u.open == v.open
 
-function MCTS.initialize!(ag::OPCSPAg, mdp::OPCSPBeliefMDP)
+function MCTS.initialize!(ag::VaroniOPCSPAg, mdp::OPCSPBeliefMDP)
     ag.anchors = Dict{Int,Vector{OPCSPBelief}}()    
 end
 
-function MCTS.assign(ag::OPCSPAg, b::OPCSPBelief)
+function MCTS.assign(ag::VaroniOPCSPAg, b::OPCSPBelief)
     found = false
-    @assert length(b.dist.mean) < 32
-    @assert b.open.limit == 256
-    @assert b.open.fill1s == false
-    if haskey(ag.anchors, b.i)
-        i_anchors = ag.anchors[b.i]
+    len = length(b.dist.mean)
+    # @assert length(b.dist.mean) < 32
+    # @assert b.open.limit == 256
+    # @assert b.open.fill1s == false
+    if haskey(ag.anchors, (b.i,b.open))
+        similar = ag.anchors[(b.i,b.open)]
         local anchor
         bestdist = ag.radius
-        for anch in i_anchors
-            if anch.remaining == b.remaining # if the open set is still the same, remaining must be
-                # if length(anch.dist.mean) >= 32
-                #     sameopen = anch.open == b.open
-                # else # this is the dirtiest thing I've ever done
-                #     @assert anch.open.fill1s == b.open.fill1s
-                #     @assert anch.open.limit == 256
-                #     @assert b.open.limit == 256
-                #     sameopen = anch.open.bits[1] == b.open.bits[1]
-                # end
-                # if sameopen
-                # if anch.open == b.open
-                if anch.open.bits[1] == b.open.bits[1]
-                    dist = 0.0
-                    for i in 1:length(anch.dist)
-                        dist += abs(anch.dist.mean[i]-b.dist.mean[i])
-                        if dist > bestdist
-                            break
-                        end
-                    end
-                    if dist <= bestdist
-                        found = true
-                        anchor = anch
-                        bestdist = dist
-                    end
+        for anch in similar
+            dist = 0.0
+            for i in 1:len
+                dist += abs(anch.dist.mean[i]-b.dist.mean[i])
+                if dist > bestdist
+                    break
                 end
+            end
+            if dist <= bestdist
+                found = true
+                anchor = anch
+                bestdist = dist
             end
         end
     else
-        i_anchors = ag.anchors[b.i] = Vector{OPCSPBelief}()
+        similar = ag.anchors[(b.i, b.open)] = Vector{OPCSPBelief}()
     end
     if !found
-        push!(i_anchors, deepcopy(b))
+        push!(similar, deepcopy(b))
         return b
     end
     return anchor
