a52275944abf1ed0966749713822060455efe2a9
Zachary Sunberg <sunbergzach@gmail.com>
2016-03-01 19:30:10 -0800

    running more tests

--- scratch/test_aggregation.jl
+++ scratch/test_aggregation.jl
@@ -1,4 +1,4 @@
-addprocs(12)
+procs = addprocs(12)
 
 using OPCSPs
 using MCTS
@@ -7,8 +7,8 @@ N = 1000
 
 problems = [gen_problem(noise=10.0,
                         p=0.2,
-                        n_nodes=8,
-                        rng=MersenneTwister(i))
+                        n_nodes=10,
+                        rng=MersenneTwister(i+1000))
             for i in 1:N]
 
 iss = initial_states(problems, rng_offset=100)
@@ -22,16 +22,21 @@ cheating = evaluate_performance(problems, iss, Cheater(), rng_offset=1000)
 mean_feedback = evaluate_performance(problems, iss, FeedbackSolver(GurobiExactSolver(multithreaded=false)), rng_offset=1000)
 @show mean(mean_feedback)
 
+heur_feedback = evaluate_performance(problems, iss, FeedbackSolver(HeuristicSolver()), rng_offset=1000)
+@show mean(heur_feedback)
+
 s = GurobiExactSolver(multithreaded=false)
 
 srng = MersenneTwister(1947)
 solver = AgUCTSolver(
-    aggregator = VoronoiOPCSPAg(10.0),
+    aggregator = VoronoiOPCSPAg(5.0),
     rollout_solver=FeedbackSolver(s),
     exploration_constant=100.0,
-    n_iterations=10000,
+    n_iterations=50000,
     rng=srng
 )
 
 @time ag = evaluate_performance(problems, iss, solver, rng_offset=1000, parallel=true)
 @show mean(ag)
+
+rmprocs(procs)
